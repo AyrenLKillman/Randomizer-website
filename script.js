6c9a235163bdc648f2ee7e027836a9aab2b935ca
Ayren Killman <ayrenkillman@gmail.com>
2024-03-24 22:57:11 +0000

    Finished

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ const textarea = document.getElementById("textarea")
 
 textarea.focus()
 
+
+// this just says "if person presses a button, then use the function "create tags" with this information
 textarea.addEventListener("keyup", (e) => {
     createTags(e.target.value)
-
+// this part just says if person presses enter, then run the randomselect function
     if(e.key === "Enter") {
-        setTimeout(() =>{
-            e.target.value = ""
-        }, 10)
-
-
+// code that was removed was the code that deleted the text box's info, this was deleted because its 
+//a student picker, and you dont want to type every single student in again after choosing one
         randomSelect()
     }
 });
-
+// this makes the span's in the HTML, though it also has a filter to remove unessesary white space
 function createTags(input) {
     const tags = input.split(",").filter(tag => tag.trim()
      !== "").map(tag => tag.trim())
@@ -30,7 +29,7 @@ function createTags(input) {
     })
 
 }
-
+//this just selects a tag and then unselects it until this function ran 30 times, and the tag it stopped on, it will keep the Highlight on it
 function randomSelect() {
     const times = 30
 
